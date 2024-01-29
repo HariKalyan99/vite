5928de0adfc4dab2dc035f1fb6876332525fff77
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-01-29 12:56:48 +0530

    useReducer hook

--- Bootstrap/context/contextTodo/Store/TodoStore.jsx
+++ Bootstrap/context/contextTodo/Store/TodoStore.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react"
+import { createContext, useReducer } from "react"
 
 
 
@@ -10,3 +10,55 @@ export const TodoStore = createContext({
 // or even the below will work
 
 // export const TodoStore = createContext([])
+
+// in terms of useReducer 
+
+// pure function 
+
+const todoItemsReducerPure = (currentState, action) => {
+    let newGetTodo = currentState;
+    if(action.type === "NEW_ITEM"){
+      const sameName = currentState.find((ele) => ele.name === action.payload.getName);
+      if(!sameName){
+        newGetTodo = [...currentState, {name: action.payload.getName, date: action.payload.getDate}];
+      }else{
+        alert("Todo already exists");
+      }
+    }else if(action.type === "DELETE_ITEM") {
+      newGetTodo = currentState.filter((todoItem) => todoItem.name !== action.payload.delName);
+    }
+    return newGetTodo;
+  }
+
+
+
+const TodoStoreContextProvider = ({children}) => {
+  const [getTodo, dispatchTodoItems] = useReducer(todoItemsReducerPure, []);
+
+    const addNewItem = (getName, getDate) => {
+        const newItemAction = {
+          type: "NEW_ITEM",
+          payload : {
+            getName,
+            getDate
+          }
+        };
+        dispatchTodoItems(newItemAction);
+      }
+    
+      const deleteTodo = (delName) => {
+        const deletItemAction = {
+          type: "DELETE_ITEM",
+          payload: {
+            delName,
+          }
+        }
+        dispatchTodoItems(deletItemAction);
+      }
+
+      return <TodoStore.Provider value={{getTodo: getTodo, handleTodoAdd: addNewItem, handleTodoDelete: deleteTodo}}>
+        {children}
+      </TodoStore.Provider>
+}
+
+export default TodoStoreContextProvider;
--- Bootstrap/context/contextTodo/src/App.jsx
+++ Bootstrap/context/contextTodo/src/App.jsx
@@ -4,38 +4,76 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import styles from './App.module.css'
 import TodoList from './components/TodoList'
 import WelcomeMessage from './components/WelcomeMessage'
-import { useState } from 'react'
-import { TodoStore } from '../Store/TodoStore'
+// import { useReducer } from 'react'
+import TodoStoreContextProvider from '../Store/TodoStore'
+
+
+// pure function 
+
+// const todoItemsReducerPure = (currentState, action) => {
+//   let newGetTodo = currentState;
+//   if(action.type === "NEW_ITEM"){
+//     const sameName = currentState.find((ele) => ele.name === action.payload.getName);
+//     if(!sameName){
+//       newGetTodo = [...currentState, {name: action.payload.getName, date: action.payload.getDate}];
+//     }else{
+//       alert("Todo already exists");
+//     }
+//   }else if(action.type === "DELETE_ITEM") {
+//     newGetTodo = currentState.filter((todoItem) => todoItem.name !== action.payload.delName);
+//   }
+//   return newGetTodo;
+// }
 
 
 function App() {
-  const [getTodo, setTodo] = useState([]);
+  // const [getTodo, setTodo] = useState([]);
+  // const [getTodo, dispatchTodoItems] = useReducer(todoItemsReducerPure, []);
 
 
-  const addNewItem = (getName, getDate) => {
-    const sameName = getTodo.find((ele) => ele.name === getName);
-    if(sameName){
-      alert("todo already exists");
-    }else{
-      setTodo([...getTodo, {name: getName, date: getDate}]);
-    }
-  }
+  // const addNewItem = (getName, getDate) => {
 
-  const deleteTodo = (delName) => {
-    const removeItem = getTodo.filter((todoItem) => todoItem.name !== delName);
-    setTodo(removeItem);
-  }
+  //   const newItemAction = {
+  //     type: "NEW_ITEM",
+  //     payload : {
+  //       getName,
+  //       getDate
+  //     }
+  //   };
+  //   dispatchTodoItems(newItemAction);
 
+
+
+  //   // const sameName = getTodo.find((ele) => ele.name === getName);
+  //   // if(sameName){
+  //   //   alert("todo already exists");
+  //   // }else{
+  //   //   setTodo([...getTodo, {name: getName, date: getDate}]);
+  //   // }
+  // }
+  
+  // const deleteTodo = (delName) => {
+  //   // const removeItem = getTodo.filter((todoItem) => todoItem.name !== delName);
+  //   // setTodo(removeItem);
+
+  //   const deletItemAction = {
+  //     type: "DELETE_ITEM",
+  //     payload: {
+  //       delName,
+  //     }
+  //   }
+  //   dispatchTodoItems(deletItemAction);
+  // }
 
   return (
-    <TodoStore.Provider value={{getTodo: getTodo, handleTodoAdd: addNewItem, handleTodoDelete: deleteTodo}}>
+    <TodoStoreContextProvider>
       <div className={styles.container}>
         <AppName appName={"Todo List"} />
         <AddTodo />
         <WelcomeMessage />
         <TodoList />
       </div>
-    </TodoStore.Provider>
+    </TodoStoreContextProvider>
   )
 }
 
